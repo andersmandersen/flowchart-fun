1a7559f26e6c26676f78e69467a61de2cc10f4c6
Anders andersen <ama@wsdev.dk>
2021-03-31 07:09:48 +0200

    WIP PWA support

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import "./slang/slang.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 Sentry.init({
   dsn:
@@ -22,6 +23,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
